cb22d2c7aa34cd2d78789378ceb11bea4692eaff
Earthchild2 <nthabiseng.fakude2@gmail.com>
2024-06-04 10:36:32 +0200

    added pizza prise

---
+++ main.js
@@ -0,0 +1,11 @@
+let pizzaPrice =200
+let wallet ;
+let sallyAmount = Number(prompt('how much does sally a have'));
+let jessyAmount = Number(prompt('how much does jessy have'));
+wallet = sallyAmount + jessyAmount
+
+if (wallet >= pizzaPrice){
+    alert ('you can buy a large pizza')
+} else {alert ('you can buy a small pizza')
+} 
+
